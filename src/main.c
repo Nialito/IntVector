2c2a596fd0bc6e509bd1035df88ce71d0a66def5
hegemonies <hegemonies@yandex.ru>
2017-02-28 13:17:10 +0700

    fix main.c

--- src/main.c
+++ src/main.c
@@ -5,8 +5,10 @@ int main()
 {
 	IntVector *v = int_vector_new(3);
 	printf("vector V = \npointer:%p\nsize:%d\ncapacity:%d\n", v->pointer,v->size,v->capacity);
+	
 	IntVector *a = int_vector_copy(v);
 	printf("\nvector A = \npointer:%p\nsize:%d\ncapacity:%d\n", a->pointer,a->size,a->capacity);
+	
 	int_vector_free(v);
 	printf("vector V = \npointer:%p\nsize:%d\ncapacity:%d\n", v->pointer,v->size,v->capacity);
 	
