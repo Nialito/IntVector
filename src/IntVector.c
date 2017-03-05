afa7cfbd8cb0c9f9ee915a7282ca09756cd2fe6e
hegemonies <hegemonies@yandex.ru>
2017-03-05 17:36:12 +0700

    Finish of labs

--- src/IntVector.c
+++ src/IntVector.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "IntVector.h"
@@ -15,7 +16,7 @@ IntVector *int_vector_copy(const IntVector *v)
 {
 	IntVector *a = malloc(sizeof(IntVector));
 	a->pointer = malloc(v->capacity * sizeof(int));
-	memcpy(a->pointer, v->pointer, sizeof(v->size));
+	memcpy(a->pointer, v->pointer, sizeof(int) * v->capacity);
 	a->size = v->size;
 	a->capacity = v->capacity;
 	return a;
@@ -26,14 +27,87 @@ void int_vector_free(IntVector *v)
 	free(v->pointer);
 	free(v);
 }
-/*
-int int_vector_get_item(const IntVector *v, size_t index);
-void int_vector_set_item(IntVector *v, size_t index, int item);
-size_t int_vector_get_size(const IntVector *v);
-size_t int_vector_get_capacity(const IntVector *v);
-int int_vector_push_back(IntVector *v, int item);
-void int_vector_pop_back(IntVector *v);
-int int_vector_shrink_to_fit(IntVector *v);
-int int_vector_resize(IntVector *v, size_t new_size);
-int int_vector_reserve(IntVector *v, size_t new_capacity);
-*/
+
+int int_vector_get_item(const IntVector *v, size_t index)
+{
+	return v->pointer[index];
+}
+
+void int_vector_set_item(IntVector *v, size_t index, int item)
+{
+	if (index <= v->capacity)
+		v->pointer[index] = item;
+}
+
+size_t int_vector_get_size(const IntVector *v)
+{
+	return v->size;
+}
+
+size_t int_vector_get_capacity(const IntVector *v)
+{
+	return v->capacity;
+}
+
+int int_vector_push_back(IntVector *v, int item)
+{
+	if (v->size < v->capacity){
+		v->pointer[v->size + 1] = item;
+		v->size++;
+	}
+	else {
+		v->capacity *= 2;
+		v->pointer = realloc(v->pointer, v->capacity * sizeof(int));
+		if (v->pointer == NULL)
+			return -1;
+			printf("!!!!%d:%d\n", item, v->size);
+		v->pointer[v->size] = item;
+		v->size++;
+	}
+	return 0;
+}
+
+void int_vector_pop_back(IntVector *v)
+{
+	if (v->size != 0) {
+		v->size--;
+		v->pointer[v->size] = 0;
+	}
+}
+
+int int_vector_shrink_to_fit(IntVector *v)
+{
+	if (v->size < v->capacity) {
+		v->capacity = v->size;
+		v->pointer = realloc(v->pointer, v->size * sizeof(int));
+		return 0;
+	}
+	return -1;
+}
+int int_vector_resize(IntVector *v, size_t new_size)
+{
+	if (new_size == v->size)
+		return 0;
+	if (new_size > v->size) {
+		v->pointer = realloc(v->pointer, new_size * sizeof(int));
+		if (v->pointer == NULL)
+			return -1;
+		for (int i = new_size - v->size; i < new_size; i++)
+			v->pointer[i] = 0;
+	} else if (new_size < v->size) {
+		return int_vector_shrink_to_fit(v);
+	}
+	v->size = new_size;
+	int_vector_reserve(v, new_size);
+	return 0;
+}
+
+int int_vector_reserve(IntVector *v, size_t new_capacity)
+{
+	if (new_capacity > v->capacity) {
+		v->capacity = new_capacity;
+		return 0;
+	}
+	return -1;
+}
+
--- src/main.c
+++ src/main.c
@@ -1,16 +1,78 @@
 #include <stdio.h>
 #include "IntVector.h"
+#define new_Line(); printf("\n");
 
-int main()
+int main(void)
 {
+	//
 	IntVector *v = int_vector_new(3);
-	printf("vector V = \npointer:%p\nsize:%d\ncapacity:%d\n", v->pointer,v->size,v->capacity);
-	
+	printf("vector V = \npointer:%p\nsize:%d\ncapacity:%d\n", v->pointer, v->size, v->capacity);
+
+	for (int i = 0; i < 3; i++) {
+		int_vector_set_item(v, i, i + 1);
+		v->size++;
+		printf("%d\t", v->pointer[i]);
+	}
+	new_Line();
+	printf("size of V after set_ item: %d", v->size);
+	new_Line();
+	//
 	IntVector *a = int_vector_copy(v);
-	printf("\nvector A = \npointer:%p\nsize:%d\ncapacity:%d\n", a->pointer,a->size,a->capacity);
-	
+	printf("\nvector A = \npointer:%p\nsize:%d\ncapacity:%d\n", a->pointer, a->size, a->capacity);
+	for (int i = 0; i < v->size; i++) {
+		printf("%d\t", a->pointer[i]);
+	}
+	new_Line();
+	new_Line();
+	/*
 	int_vector_free(v);
-	printf("vector V = \npointer:%p\nsize:%d\ncapacity:%d\n", v->pointer,v->size,v->capacity);
 	
-	return 0;
+	printf("vector V = \npointer:%p\nsize:%d\ncapacity:%d\n", v->pointer,v->size,v->capacity);
+	printf("v: after free\n");
+	for (int i = 0; i < v->size; i++) {		
+		printf("%d\t", v->pointer[i]);
+	}
+	new_Line();
+	*/
+	int_vector_free(a);
+	printf("a: after free\n");
+	for (int i = 0; i < a->size; i++) {
+		printf("%d\t", a->pointer[i]);
+	}
+	
+	new_Line();
+
+	printf("v:\n");
+	for (int i = 0; i < v->size; i++) {
+		int_vector_set_item(v, i, i + 1);
+		printf("%d\t", int_vector_get_item(v, i));
+	}
+	
+	new_Line();
+	
+	//
+	printf("get_size v: %d", (int) int_vector_get_size(v));
+	new_Line();
+	//
+	int_vector_push_back(v, 66);
+	//
+	printf("push_back v:\n");
+	for (int i = 0; i < v->size; i++)
+		printf("%d\t", int_vector_get_item(v, i));
+	new_Line();
+	printf("capacity: %d\n size: %d", v->capacity, v->size);
+	new_Line();
+	int_vector_pop_back(v);
+	for (int i = 0; i < v->size; i++)
+		printf("!%d\t", int_vector_get_item(v, i));
+		new_Line();
+	
+	printf("shrink: %d", int_vector_shrink_to_fit(v));
+	new_Line();
+	printf("- %d", v->capacity);
+	new_Line();
+	
+	printf("size = %d. capacity = %d.\n", v->size, v->capacity);
+	printf("%d\n", int_vector_resize(v, 10));
+	printf("size = %d. capacity = %d.\n", v->size, v->capacity);
 }
